0d94e546a2aade22ed290a90730ad861b37a5969
Shane Murphy <smurf6258@yahoo.com>
2016-12-07 11:33:53 -0500

    Began adding if else statements

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -6,12 +6,23 @@ puts "What year were you born?"
 year = gets.chomp
 current_year = "2016"
 age = "current_year - #{year}" 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?" (y/n)
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)?"
 bread = gets.chomp
-puts "Would you like to enroll in the company’s health insurance?"(y/n)
+puts "Would you like to enroll in the company’s health insurance? (y/n)?"
 insurance = gets.chomp
 wolves_like_sunshine = true
 wolves_like_garlic = true
 vampires_like_sunshine = false
 vampires_like_garlic = false
-if age != yearsold
+if age = yearsold
+	puts "#{age} is true"
+else 
+	puts "#{age} is false"
+end
+if 
+	
+end
+
+	
+
+	
