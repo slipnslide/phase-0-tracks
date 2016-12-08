53576da444c5f1ad4d04fd73ff613630c8271b50
Shane Murphy <smurf6258@yahoo.com>
2016-12-07 23:40:52 -0500

    cleaned up output of program

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,15 +1,15 @@
 puts "What is your hamsters name?"
 name = gets.chomp
-puts "your hamsters name is #{name}"
+
 
 puts "How noisy is your hamster? (1-10)"
 volume = gets.chomp
 volume.to_i(base=10)
-puts "#{volume}"
+
 
 puts "What is your hamsters fur color"
 color = gets.chomp
-puts "#{color} is really cool"
+
 
 puts "Is your hamster a good candidate for adoption (y/n)"
 
@@ -23,15 +23,20 @@ puts "Is your hamster a good candidate for adoption (y/n)"
  end
 
 
-puts "#{adoption}"
+
 
 puts "What is the estimated age of your hamster?"
 age = gets.chomp
 if age.empty?
 	nil
 else 
-	age = age.to_i
+	age = "age".to_i
 end
-p age
 
+puts "#{name} Adoption Profile"
+puts "Your hamsters name is #{name}"
+puts "#{name} has a #{volume} noise level"
+puts "#{name} has #{color} fur "
+puts "Is #{name} a candidate: #{adoption}"
+puts "We think #{name}'s age is #{age}"
 
