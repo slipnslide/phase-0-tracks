8a341b225a14f45fde140decb7a77e97f95ab7d4
Shane Murphy <smurf6258@yahoo.com>
2016-12-08 04:17:06 -0500

    added loop for employees

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,64 +1,71 @@
-puts "What is your name?"
-name = gets.chomp
+puts "How many employees will be processed today?"
+x = gets.chomp.to_i
+until x == 0
 
-puts "How old are you?"
-age = gets.chomp
+
+  puts "What is your name?"
+  name = gets.chomp
+
+  puts "How old are you?"
+  age = gets.chomp
+    
+  puts "What year were you born?"
+  year = gets.chomp
+
+
+
+  current_year = 2016
+  yearsold = current_year - year.to_i
+
+
+  puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)?"
+  bread = gets.chomp
+
+  if bread == "y"
+   bread = true
   
-puts "What year were you born?"
-year = gets.chomp
-
-
-
-current_year = 2016
-yearsold = current_year - year.to_i
-
-
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)?"
-bread = gets.chomp
-
-if bread == "y"
- bread = true
+  elsif bread == "n"
+   bread = false
   
-elsif bread == "n"
-  bread = false
-  
-else
-  puts "you broke it start over"
-end
+  else
+    puts "you broke it start over"
+  end
 
-puts "Would you like to enroll in the company’s health insurance? (y/n)?"
-insurance = gets.chomp
+  puts "Would you like to enroll in the company’s health insurance? (y/n)?"
+  insurance = gets.chomp
 
-if insurance == "y"
+  if insurance == "y"
    insurance = true
   
-elsif insurance == "n"
+  elsif insurance == "n"
    insurance = false
   
-else
-  puts "you broke it start over"
-end
+  else
+    puts "you broke it start over"
+  end
 
 
 
- if "#{yearsold}" == "#{age}" && ("#{bread}" && "#{insurance}")
+   if "#{yearsold}" == "#{age}" && ("#{bread}" && "#{insurance}")
 	 puts "Probably not a vampire."
-	elsif ("#{yearsold}" == "#{age}" && "#{bread}") && "#{insurance}"
-	 puts "Probably a vampire."
-	elsif ("#{yearsold}" == "#{age}" && "#{insurance}") && "#{bread}"
-	 puts "Probably a vampire."
-	elsif "#{yearsold}" != "#{age}" && ("#{bread}" || "#{insurance}")
-	 puts "Probably a vampire."
-	elsif "#{yearsold}" != "#{age}" && ("#{bread}"  && "#{insurance}")
-	 puts "Almost certainly a vampire."
-	elsif "#{name}" == "Drake Cula ; Tu Fang" && !("#{yearsold}" != "#{age}" && "#{bread}" && "#{insurance}")
-		puts "definitely a vampire"
-	else
-		puts "Results Inconclusive"
-		
-	
+  	elsif ("#{yearsold}" == "#{age}" && "#{bread}") || "#{insurance}"
+	   puts "Probably a vampire."
+	  elsif ("#{yearsold}" == "#{age}" && "#{insurance}") || "#{bread}"
+	   puts "Probably a vampire."
+	  elsif ("#{yearsold}" != "#{age}" && "#{bread}") || "#{insurance}"
+	   puts "Probably a vampire."
+  	elsif ("#{yearsold}" != "#{age}" && "#{insurance}") || "#{bread}"
+	   puts "Probably a vampire."
+    elsif "#{yearsold}" != "#{age}" && ("#{bread}"  && "#{insurance}")
+	   puts "Almost certainly a vampire."
+	  elsif "#{name}" == ("Drake Cula" || "Tu Fang") && ("#{yearsold}" == "#{age}" && "#{bread}" && "#{insurance}")
+	  	puts "definitely a vampire"
+  	else
+	  	puts "Results Inconclusive"
 	end
-
+	x -= 1
+end
+puts "All done!"
 
 
 
