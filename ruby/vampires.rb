adfd45288bea976dcbd299696b51a4a031ec085b
Shane Murphy <smurf6258@yahoo.com>
2016-12-08 01:31:26 -0500

    making progress on outputs

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,25 +1,37 @@
 puts "What is your name?"
 name = gets.chomp
+
 puts "How old are you?"
-yearsold = gets.chomp
+age = gets.chomp
+  
 puts "What year were you born?"
 year = gets.chomp
-current_year = "2016"
-age = "current_year - #{year}" 
+
+
+
+current_year = 2016
+yearsold = current_year - year.to_i
+
+
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)?"
 bread = gets.chomp
+
+
 puts "Would you like to enroll in the company’s health insurance? (y/n)?"
 insurance = gets.chomp
 
 
 
- if "#{age}" == "#{yearsold}" && "#{bread}" == y && "#{insurance}" == y
+
+ if "#{age}" == "#{yearsold}" && "#{bread}" == "y" && "#{insurance}" == "n"
 	 puts "Probably not a vampire."
-	elsif "#{age}" != "#{yearsold}" && ("#{bread}" == n || "#{insurance}" == n)
+	elsif "#{age}" != "#{yearsold}" && ("#{bread}" == "y" || "#{insurance}" == "n")
 	 puts "Probably a vampire."
-	elsif "#{age}" != "#{yearsold}" && ("#{bread}" == y || "#{insurance}" == y)
+	elsif "#{age}" != "#{yearsold}" && ("#{bread}" == "n" || "#{insurance}" == "y")
+	 puts "Probably a vampire."
+	elsif "#{age}" != "#{yearsold}" && ("#{bread}" == "n" && "#{insurance}" == "n")
 	 puts "Almost certainly a vampire."
-	elsif "#{name}" == "Drake Cula ; Tu Fang" && !("#{age}" != "#{yearsold}" && "#{bread}" == n && "#{insurance}" == n)
+	elsif "#{name}" == "Drake Cula ; Tu Fang" && !("#{age}" != "#{yearsold}" && "#{bread}" == "n" && "#{insurance}" == "n")
 		puts "definitely a vampire"
 	else
 		puts "Results Inconclusive"
