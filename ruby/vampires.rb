a71f0093078995aed369bc104d597b9ec7850d9a
Shane Murphy <smurf6258@yahoo.com>
2016-12-07 19:54:24 -0500

    Trying to make if else use latest condition

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -12,17 +12,20 @@ puts "Would you like to enroll in the company’s health insurance? (y/n)?"
 insurance = gets.chomp
 
 
-if "#{age} == #{yearsold}" and "#{bread} == y" and "#{insurance} == y"
-	puts "Probably not a vampire."
-	elsif "#{age} != #{yearsold}" and "#{bread} == n" or "#{insurance} == n"
-	puts "Probably a vampire."
-	elsif "#{age} != #{yearsold}" and "#{bread} == n" and "#{insurance} == n"
+
+def vamp_test()
+	if "#{age} == #{yearsold}" and "#{bread} == y" and "#{insurance} == y"
+	 puts "Probably not a vampire."
+		elsif "#{age} !== #{yearsold}" and "#{bread} === n" or "#{insurance} == n"
+	 puts "Probably a vampire."
+		elsif "#{age} != #{yearsold}" and "#{bread} == n" and "#{insurance} == n"
 	 puts "Almost certainly a vampire."
-	elsif "#{name} == Drake Cula or Tu Fang" "#{age} == #{yearsold}" and "#{bread} == y" and "#{insurance} == y"
+		elsif "#{name} == Drake Cula ; Tu Fang" "#{age} == #{yearsold}" and "#{bread} == y" and "#{insurance} == y"
 		puts "definitely a vampire"
-	else
+		else
 		puts "Results Inconclusive"
 		
+	end
 end
 
 
