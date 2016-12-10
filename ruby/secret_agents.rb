ba01fd7ec9687436c7c691c67ae6dbb939a96703
Shane Murphy <smurf6258@yahoo.com>
2016-12-10 15:27:41 -0500

    Tidying up code bits

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,54 +1,56 @@
-def encrypt_loop(encrypt)
-    index = 0
-    while index < encrypt.length
-        if encrypt[index] != " "
-            encrypt[index] = encrypt[index].next
-            index += 1
-        else
-            index += 1
-        end
-    end
-    p encrypt
+def encrypt(string)
+ counter = 0
+ while counter < string.length
+   if string[counter] == 'z'
+     string[counter] = 'a'
+     
+   elsif string[counter] == " "
+     string[counter] = string[counter]
+       
+   else
+     string[counter] = string[counter].next
+   end
+     counter = counter + 1
+ end
+   return string
 end
 
-def decrypt_loop(decrypt)
-    new_string = ""
-    i = 0
-    #iderate through the String
-    while i < decrypt.length
-        if decrypt[i] != " "
-            # get the index of the alphabet String
-            str ="abcdefghijklmnopqrstuvwxyz"
-            new_value = str.index(decrypt[i])
-            #change to the new letter
-            new_value = new_value -1
-            str[new_value]
-            #add letter to new string
-            new_string = new_string + str[new_value]
-            i += 1
-        else
-            new_string = new_string + " "
-            i += 1
-        end
-    end
-    p new_string
-end
-# Our Driver code
-# Ask user for encryption or decryption
+def decrypt(string)
+ alphabet = "abcdefghijklmnopqrstuvwxyz"
+ counter = 0
+ while counter < string.length
 
-puts "Would you like to encrypt or decrypt?"
+   if string[counter] == " "
+     string[counter] == string[counter]
+   else
+     
+   string[counter] = alphabet[(alphabet.index(string[counter]) - 1)]
+
+   end
+
+   counter = counter + 1
+
+ end
+ return string
+end
+
+puts 'Would you like to encrypt or decrypt?'
 answer = gets.chomp
-    #based on choice run corresponding method
-if answer == "encrypt"
-    puts "What would you like to encrypt?"
-    encrypt = gets.chomp
-    encrypt_loop(encrypt)
 
-elsif answer == "decrypt"
-    puts "what would you like to decrypt?"
-    decrypt = gets.chomp
-    decrypt_loop(decrypt)
-    # If invalid answer "bye, bye"
+if answer == 'encrypt'
+ puts 'please give me the password to encrypt.'
+ password_en = gets.chomp
+ result = encrypt(password_en)
+ puts "Your encrypted password is is #{result}"
+elsif answer == 'decrypt'
+ puts "please give me the password to decrypt."
+ password_de = gets.chomp
+ result = decrypt(password_de)
+ puts "Your decrypted password  is #{result}"
 else
-    puts "Sorry, bye"
+ puts 'wrong input try again'
 end
+#decrypt(encrypt("swordfish"))
+=begin
+this bit of code works because of the order of operations. what is inside of the () gets evaluated first, after that the only thing left is the encrypted string and the decrypt which is why it works.
+=end
