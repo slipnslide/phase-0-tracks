9bc1aa8c3dbf10f6a0fcbd29afc44793efb37f5c
Shane Murphy <smurf6258@yahoo.com>
2016-12-13 22:45:51 -0500

    Added P so statements print

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,34 +4,34 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
- "iNvEsTiGaTiOn".swapcase 
+p "iNvEsTiGaTiOn".swapcase 
 # => “InVeStIgAtIoN”
 
- "zom".insert(1, "o")
+p "zom".insert(1, "o")
  #=> “zoom”
 
- "enhance".center(16)
+p "enhance".center(16)
  #=> "    enhance    "
 
- "Stop! You’re under arrest!".upcase
+p "Stop! You’re under arrest!".upcase
 #=> "STOP! YOU’RE UNDER ARREST!"
 
- "the usual".insert(-1, " suspects")
+p "the usual".insert(-1, " suspects")
 #=> "the usual suspects"
 
-" suspects".prepend("the usual")
+p " suspects".prepend("the usual")
 # => "the usual suspects"
 
- "The case of the disappearing last letter".chop
+p "The case of the disappearing last letter".chop
  #=> "The case of the disappearing last lette"
 
-"The mystery of the missing first letter".delete "T"
+p "The mystery of the missing first letter".delete "T"
 #=> "he mystery of the missing first letter"
 
- "Elementary,    my   dear        Watson!".squeeze(" ")
+p "Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
- "z".bytes.join
+p "z".ord
  #=> 122 
 
 
@@ -41,5 +41,5 @@
 # (What is the significance of the number 122 in relation to the character z?)
 #according to the ASCII code "American Standard Code for Information Interchange z = 122 if you hold alt and press 122 you get a lowercase z"
 
- "How many times does the letter 'a' appear in this string?".count "a"
+p "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
