7ac45d5ad7f01c3f2beb2ca2a4007ef46f6a55cb
Shane Murphy <smurf6258@yahoo.com>
2016-12-16 18:48:11 -0500

    Nested Data First commit

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,57 @@
+Hotel_Madness = {
+	top_floor: {
+		vip: {
+			rich_guy: 50_Cent,
+			owner: Billy the kid,
+
+			},
+			service: [
+				"open bar"
+				"room cleaning"
+				"clothes washed"
+				"bags carried"
+				"food"
+			]
+	},
+	middle_floor: {
+		average_guest: {
+			guest1: Business Guy,
+			guest2: Family,
+			guest3: kids partying,
+			},
+			type_of_service: [
+				"room cleaning"
+				"food"
+				"bags carried"
+			]
+	},
+	bottom_floor: {
+		front_Desk: {
+				greeter: Angelica,
+				doorman: Roger,
+				bag_boy: Sammy,
+			},
+			jobs: [
+				"Greeters"
+				"Doormen"
+				"Bag Carrier"
+			]
+	},
+	basement: {
+		staff: {
+			maid: Sarah,
+			waiter: Timmy,
+			dish_washer: Jose,
+			chef: Luise,
+
+			},
+		jobs: [
+			"Clean"
+			"Serve"
+			"Wash"
+			"Cook"
+
+		]
+
+	},
+}
