fbaf0b6c1947fe240a85868a2cf0ff47f48850de
Shane Murphy <smurf6258@yahoo.com>
2016-12-16 19:07:21 -0500

    Fixed commas

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,57 +1,58 @@
 Hotel_Madness = {
 	top_floor: {
 		vip: {
-			rich_guy: 50_Cent,
-			owner: Billy the kid,
+			rich_guy: "50 Cent",
+			owner: "Billy the kid"
 
 			},
 		service: [
-				"open bar"
-				"room cleaning"
-				"clothes washed"
-				"bags carried"
+				"open bar",
+				"room cleaning",
+				"clothes washed",
+				"bags carried",
 				"food"
 			]
 	},
 	middle_floor: {
 		average_guest: {
-			guest1: Business Guy,
-			guest2: Family,
-			guest3: kids partying,
+			guest1: "Business Guy",
+			guest2: "Family",
+			guest3: "kids partying"
 			},
 		type_of_service: [
-				"room cleaning"
-				"food"
+				"room_cleaning",
+				"food",
 				"bags carried"
 			]
 	},
 	bottom_floor: {
 		front_Desk: {
-				greeter: Angelica,
-				doorman: Roger,
-				bag_boy: Sammy,
+				greeter: "Angelica",
+				doorman: "Roger",
+				bag_boy: "Sammy"
 			},
 		jobs: [
-				"Greeters"
-				"Doormen"
+				"Greeters",
+				"Doormen",
 				"Bag Carrier"
 			]
 	},
 	basement: {
 		staff: {
-			maid: Sarah,
-			waiter: Timmy,
-			dish_washer: Jose,
-			chef: Luise,
+			maid: "Sarah",
+			waiter: "Timmy",
+			dish_washer: "Jose",
+			chef: "Luise"
 
 			},
 		jobs: [
-			"Clean"
-			"Serve"
-			"Wash"
+			"Clean",
+			"Serve",
+			"Wash",
 			"Cook"
 
 		]
 
-	},
+	}
 }
+p Hotel_Madness[:bottom_floor][:jobs].last
