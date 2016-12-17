fc7a77ffc7c9c1b3d737f8b7701e5886d1c30878
Shane Murphy <smurf6258@yahoo.com>
2016-12-17 06:00:36 -0500

    Added comma after operation so program would run and printed examples to test that they work

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -8,9 +8,12 @@
 #You can use __send__ if the name send clashes with an existing method in obj. 
 #when the method is identified by a string, the string is converted to a symbol.
 def calculate(x, operation, y)
-	x.send(operation y)
+	x.send(operation, y)
 end
-calculate(4, "+", 5)
+p calculate(4, "+", 5)
+p calculate(3, "*", 2)
+p calculate(12, "/", 3)
+p calculate(3, "-", 2)
 
 
 
