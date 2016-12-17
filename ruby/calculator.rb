a00883b41967cefd81d4d8392f87cb3ec3a59a85
Shane Murphy <smurf6258@yahoo.com>
2016-12-17 05:57:48 -0500

    Defined method wrote pseudocode

---
+++ ruby/calculator.rb
@@ -0,0 +1,16 @@
+
+#def calculator method
+#lookup how to add operation
+
+#lookup how to pass operation to string
+#".send"
+#Invokes the method identified by symbol, passing it any arguments specified.
+#You can use __send__ if the name send clashes with an existing method in obj. 
+#when the method is identified by a string, the string is converted to a symbol.
+def calculate(x, operation, y)
+	x.send(operation y)
+end
+calculate(4, "+", 5)
+
+
+
