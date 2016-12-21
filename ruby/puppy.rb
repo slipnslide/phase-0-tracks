5b8f5ed38616d74670b6f789b916e831aa6e58a2
Shane Murphy <smurf6258@yahoo.com>
2016-12-21 12:26:07 -0500

    pseudocode puppy characteristics

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+#SPECIES ------
+# Dog
+
+#characteristics ----
+#height : varies
+#weight: varies
+# eyecolor: varies
+#name: varies
+#color: varies
+
+#BEHAVIORS----
+#Active
+#Lovable
+#Cuddle
+#Nibble
