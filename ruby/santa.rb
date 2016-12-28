2197181f29701ebeca874b08cfb12a39dd7e1ee6
Shane Murphy <smurf6258@yahoo.com>
2016-12-28 18:19:15 -0500

    Added all the methods and driver code for santa

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,20 +1,78 @@
+
+
 class Santa
+	#getter and setter methods refactored
+	attr_reader :ethnicity
+	attr_accessor :gender, :age
+
+#initialize method
+	def initialize(gender, ethnicity)
+	  puts "Initializing Santa instance ..."
+	  @gender = gender
+	  @ethnicity = ethnicity 
+	  @age = 0
+	  @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	end
+#speak method that says "Ho, ho, ho! Haaaappy holidays!"
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
-
-	def eat_milk_and_cookies(str)
-		puts "That was a good #{str}!"
+#cookie method that takes a string and returns "That was a good #{cookie}!"
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}!"
 	end
-
-	def initialize
-	  puts "Initializing Santa instance ..."
+#celebrate_birthday should age Santa by one year.
+	def celebrate_birthday
+		@age += 1
 	end
-
+#get_mad_at can take a reindeer's name as an argument, and move that reindeer in last place in the reindeer rankings
+	def get_mad_at(reindeer_name)
+		@reindeer_ranking.delete(reindeer_name)
+		@reindeer_ranking << reindeer_name
+	end
 
 end
-santa1 = Santa.new
-santa1.speak
-santa1.eat_milk_and_cookies("snickerdoodler")
-santa2 = Santa.new
-santa2.eat_milk_and_cookies("chocolate")
+
+
+#driver code
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+100.times do 
+#found .sample in ruby doc which allows an element to be chosen randomly from an array
+  santa = Santa.new(example_genders.sample, example_ethnicities.sample)
+  santa.age= (rand(140))
+  #to put santa back into an array
+  #santas << santa
+  p santa
+end
+
+
+#old test code
+#santa1 = Santa.new
+#santa1.speak
+#santa1.eat_milk_and_cookies("snickerdoodler")
+#santa2 = Santa.new
+#santa2.eat_milk_and_cookies("chocolate")
+
+#old getter and setter methods
+
+#Add two "getter" methods as well:
+#The @gender attribute should have a setter method that allows 
+# @gender to be reassigned from outside the class definition.
+
+#getter method for age and ethnicity
+
+#	def age
+#		@age
+#	end
+
+#	def ethnicity
+#		@ethnicity
+#	end
+
+	#setter method for gender
+#	def gender=(new_gender)
+#		@gender = new_gender
+#	end
