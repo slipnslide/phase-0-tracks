1bb67d9a3726608cbea9897996b6cbe56ba60303
Shane Murphy <smurf6258@yahoo.com>
2016-12-28 19:05:26 -0500

    Added shout module and mixin modules

---
+++ ruby/shout.rb
@@ -0,0 +1,11 @@
+module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+  def self.yelling_happily(words)
+  	words + "!!!!" + " =]"
+  end
+
+end
+p Shout.yelling_happily("Yay!")
+p Shout.yell_angrily("GRRRRR")
