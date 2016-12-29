33d2bc7e2fc18621b9bf81acb3c2decbfb8c3c6c
Shane Murphy <smurf6258@yahoo.com>
2016-12-28 19:30:06 -0500

    Finished shout.rb

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,37 @@
+#module Shout
+ # def self.yell_angrily(words)
+  #  words + "!!!" + " :("
+  #end
+ # def self.yelling_happily(words)
+ # 	words + "!!!!" + " =]"
+ # end
+
+#end
+#p Shout.yelling_happily("Yay!")
+#p Shout.yell_angrily("GRRRRR")
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
-  def self.yelling_happily(words)
-  	words + "!!!!" + " =]"
-  end
+	def Angry_driver(words)
+		words + " How are you so bad at driving!!!!!!"
+	end
+	def Happy_driver(words)
+		words + " Have a nice day!"
+	end
 
 end
-p Shout.yelling_happily("Yay!")
-p Shout.yell_angrily("GRRRRR")
+
+
+class Driver
+	include Shout
+end
+
+class Other_driver
+	include Shout
+end
+
+driver1 = Driver.new
+driver2 = Other_driver.new
+
+p driver1.Angry_driver("I just don't understand...")
+p driver2.Happy_driver("You are an amazing person!")
+
