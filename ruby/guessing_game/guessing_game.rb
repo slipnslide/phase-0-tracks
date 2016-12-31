2a1c48bc462a52dd7b9a35829fe0fa3d7cd5763a
Shane Murphy <smurf6258@yahoo.com>
2016-12-31 13:06:44 -0500

    Finished guessing game added clear so word doesn't show to player 2, changed game = GuessFun.new(word_to_guess)

--- ruby/guessing_game/guessing_game.rb
+++ ruby/guessing_game/guessing_game.rb
@@ -1,19 +1,21 @@
 class GuessFun
+	#create attr_reader for all variables you need to access outside of scope
 	attr_reader :guesses, :word, :guessed_array, :underscore, :underscore_array
+	#initialize everything that has to be done to the word and setup instance variables
 	def initialize(word)
 		@word = word.split("")
-		@guesses = word.length * 2
-		@underscore_array = @word.map { |e| "_" }
+		@guesses = @word.length * 2
+		@underscore_array = @word.map {|x| "_" }
 		@underscore = @underscore_array.join(" ")
 		@guessed_array = []
 	end
-
 	def guess_a_letter(letter)
+		#Create a statement so that already guessed letters don't count against you
 		if @guessed_array.include?(letter)
 			p "You already tried that letter silly...."
 			return
 		end
-
+		#create an extra empty array to replace the letters guessed if correctly guessed
 		if @word.include?(letter)
 			replace_array = @word.each_index.select{|n| @word[n] == letter}
 			replace_array.reverse.each {|i| @underscore_array.delete_at(i)}
@@ -22,7 +24,36 @@ class GuessFun
 		else
 			@underscore
 		end
+		#after guessing update guessed array and guess counter
 		@guessed_array << letter
 		@guesses = @guesses - 1
 	end
 end
+
+#user console
+puts "Please enter a word for your friend to guess!"
+word_to_guess = gets.chomp
+game = GuessFun.new(word_to_guess)
+system "clear"
+
+while !(game.guesses == 0 || game.underscore_array == game.word)
+	p "You get #{game.guesses} guesses Good Luck!"
+	p "Please try a letter"
+	letter = gets.chomp.downcase
+	game.guess_a_letter(letter)
+	#print the underscore not the underscore_array
+	p game.underscore
+end
+
+if game.guesses == 0 && game.underscore_array != game.word
+	puts "The word was #{game.word.join.upcase} Better luck next time loser!"
+else
+	puts "I guess #{game.word.join.upcase} just wasn't good enough... you win..."
+end
+
+
+
+
+
+
+
