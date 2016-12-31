6143eca9e56ca2ed4af8e052d90c3b9c4a68e23a
Shane Murphy <smurf6258@yahoo.com>
2016-12-31 12:28:23 -0500

    Finished fixing errors

--- ruby/guessing_game/guessing_game.rb
+++ ruby/guessing_game/guessing_game.rb
@@ -8,6 +8,21 @@ class GuessFun
 		@guessed_array = []
 	end
 
+	def guess_a_letter(letter)
+		if @guessed_array.include?(letter)
+			p "You already tried that letter silly...."
+			return
+		end
 
-
+		if @word.include?(letter)
+			replace_array = @word.each_index.select{|n| @word[n] == letter}
+			replace_array.reverse.each {|i| @underscore_array.delete_at(i)}
+			replace_array.each {|i| underscore_array.insert(i, letter)}
+			@underscore = @underscore_array.join(" ")
+		else
+			@underscore
+		end
+		@guessed_array << letter
+		@guesses = @guesses - 1
+	end
 end
